fa3ce41d7da2d4d79215f8f9ce4aab8c941b0b57
Bill Mill <bill.mill@gmail.com>
2009-03-15 16:27:25 -0400

    horizontal edge detection works

--- actions/edge.js
+++ actions/edge.js
@@ -1,8 +1,13 @@
 Pixastic.Actions.edge = {
+  contain : function(val, min, max) {
+    return Math.max(min, Math.min(val, max));
+  },
+
   each_3x3 : function(data, w, h, visit, avg_function) {
     var w4 = w*4;
     var y = h;
     var end = w4*h;
+    copy = data.slice();
     do {
       var offtopY = (y-2)*w4;
       var offsetY = (y-1)*w4;
@@ -12,13 +17,21 @@ Pixastic.Actions.edge = {
         var offtop = offtopY + (x-1)*4;
         var offset = offsetY + (x-1)*4;
         var offbot = offbotY + (x-1)*4;
-        var g = function (offset) {
-          return offset > 0 && offset < end ? data[offset] : 0;
-        }
-        var newval = visit(g(offtop-4), g(offtop), g(offtop+4),
-                           g(offset-4), g(offset), g(offset+4),
-                           g(offbot-4), g(offbot), g(offbot+4));
-        data[offset] = data[offset+1] = data[offset+2] = newval;
+
+        var z1 = x > 1 && offtop > 0 ? copy[offtop-4] : 0;
+        var z2 = offtop > 0 ? copy[offtop] : 0;
+        var z3 = x < w && offtop > 0 ? copy[offtop+4] : 0;
+
+        var z4 = x > 1 ? copy[offset-4] : 0;
+        var z5 = copy[offset];
+        var z6 = x < w ? copy[offset+4] : 0;
+
+        var z7 = x > 1 && offbot < end ? copy[offbot-4] : 0;
+        var z8 = offbot < end ? copy[offbot] : 0;
+        var z9 = x < w && offbot < end ? copy[offbot+4] : 0;
+
+        var newval = visit(z1, z2, z3, z4, z5, z6, z7, z8, z9);
+        data[offset] = data[offset+1] = data[offset+2] = this.contain(newval, 0, 255); 
       } while (--x);
     } while (--y);
   },
@@ -29,17 +42,8 @@ Pixastic.Actions.edge = {
       var rect = params.options.rect;
       var w = rect.width;
       var h = rect.height;
-      var first = true;
       this.each_3x3(data, w, h, function(z1, z2, z3, z4, z5, z6, z7, z8, z9) {
         w = [-1, -2, -1, 0, 0, 0, 1, 2, 1];
-        if (first) {
-          console.log("first pix: ", z1, z2, z3, z4, z5, z6, z7, z8, z9);
-          console.log("result: ", w[0]*z1 + w[1]*z2 + w[2]*z3 +
-               w[3]*z4 + w[4]*z5 + w[5]*z6 +
-               w[6]*z7 + w[7]*z8 + w[8]*z9);
-
-          first = false;
-        }
         return w[0]*z1 + w[1]*z2 + w[2]*z3 +
                w[3]*z4 + w[4]*z5 + w[5]*z6 +
                w[6]*z7 + w[7]*z8 + w[8]*z9;
